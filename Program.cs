469e36ed03e509088a4784e1ca035fbb58f3d46f
Oguzhan Turkmen <99134453+oguzhanturkmen2@users.noreply.github.com>
2022-02-06 23:24:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sample = "aaa.bbb";
+            string[] a = new string[100];
+            a = sample.Split(".");
+
+            for (int i = 0; i < a.Length; i++)
+                Console.WriteLine(a[i]);
+
+        }
+    }
+}
